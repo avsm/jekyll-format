57aedcefc91a7740be7a1fcd21d3b337f5182152
Anil Madhavapeddy <anil@recoil.org>
2016-10-25 12:28:09 +0100

    fix typo

--- src/jekyll_format.mli
+++ src/jekyll_format.mli
@@ -32,7 +32,7 @@ val body : t -> body
 
 (** {1 Conversion functions} *)
 
-val of_string : string -> (t, [> Rresult.R.msg ]) Result.t
+val of_string : string -> (t, [> Rresult.R.msg ]) Result.result
 (** [of_string t] parses a Jekyll-format blog post and either returns a {!t}
     or signals an error in the result. *)
 
